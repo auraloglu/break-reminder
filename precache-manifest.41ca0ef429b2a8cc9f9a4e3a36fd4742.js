c3ebc8b47031cb18ad865b381838071f7b319f05
cappydh <ahmeturaloglu@msn.com>
2020-02-29 16:32:28 +0300

    Updates

---
+++ precache-manifest.41ca0ef429b2a8cc9f9a4e3a36fd4742.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e55d9e61d5b160edf2e01f6eeb74467",
+    "url": "/break-reminder/index.html"
+  },
+  {
+    "revision": "eaad341d17e22684b1b2",
+    "url": "/break-reminder/static/css/main.ac9557bf.chunk.css"
+  },
+  {
+    "revision": "890d811debf86ce9ea52",
+    "url": "/break-reminder/static/js/2.789c0a29.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/break-reminder/static/js/2.789c0a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaad341d17e22684b1b2",
+    "url": "/break-reminder/static/js/main.e6ed43a4.chunk.js"
+  },
+  {
+    "revision": "efda19ad709e78492ade",
+    "url": "/break-reminder/static/js/runtime-main.8c95c036.js"
+  },
+  {
+    "revision": "580d0778ad254335be45bf58bb449f43",
+    "url": "/break-reminder/static/media/Raleway-Regular.580d0778.ttf"
+  }
+]);
